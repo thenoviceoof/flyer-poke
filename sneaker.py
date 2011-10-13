313ccc3cedacee1b113e97e2cc335912d55efe67
Nathan Hwang <beta.entity.k@gmail.com>
2011-10-12 21:36:57 -0400

    Got pdf uploading and downloading working

--- sneaker.py
+++ sneaker.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import db
+from google.appengine.api import mail
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -6,7 +7,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 class Flyer(db.Model):
     id = db.StringProperty()
     flyer = db.BlobProperty()
-    recpients = db.TextProperty()
+    recipients = db.TextProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
 class Index(webapp.RequestHandler):
@@ -17,13 +18,24 @@ class Index(webapp.RequestHandler):
 class Upload(webapp.RequestHandler):
     def post(self):
         flyer = Flyer()
-        flyer.recpients = self.request.get("content")
+        flyer.recipients = self.request.get("content")
         pdf = self.request.get("flyer")
-        flyer.avatar = db.Blob(pdf)
+        flyer.flyer = db.Blob(pdf)
+        flyer.put()
+        flyer.id = str(flyer.key().id())
         flyer.put()
 
         values = {}
         self.response.out.write(template.render("templates/finish.html", values))
+
+class Admin(webapp.RequestHandler):
+    def get(self):
+        flyer_req = db.GqlQuery("SELECT * "
+                                "FROM Flyer")
+        flyers = flyer_req.fetch(10)
+
+        values = {"flyers":flyers}
+        self.response.out.write(template.render("templates/list.html", values))
 
 class Pdf(webapp.RequestHandler):
     def get(self, id):
@@ -31,18 +43,19 @@ class Pdf(webapp.RequestHandler):
                              "FROM Flyer "
                              "WHERE id=:1",
                              id)
-        flyer = flyers.fetch(1)
+        flyer = flyers.fetch(1)[0]
 
         if flyer.flyer:
             self.response.headers['Content-Type'] = "application/pdf"
             self.response.out.write(flyer.flyer)
         else:
-            self.error(404)        
+            self.error(404)
 
 
 application = webapp.WSGIApplication(
     [('/', Index),
      ('/upload', Upload),
+     ('/admin', Admin),
      ('/pdf/(\d*)',Pdf)],
     debug=True)
 
