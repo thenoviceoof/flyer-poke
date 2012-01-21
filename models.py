2808f01227784e9c3649a56ec29d2fcff0bbb3db
Nathan Hwang <beta.entity.k@gmail.com>
2012-01-20 23:28:26 -0500

    Commented model.py better, added small bits and pieces

--- models.py
+++ models.py
@@ -4,39 +4,47 @@ from google.appengine.api import users
 ################################################################################
 # Organizational models
 
-# key_name replicates name
+# key replicates name, but as a slug (letters/numbers only)
 class Club(db.Model):
     name = db.StringProperty()
+    # for a cron job that emails the super-admin new clubs
+    new  = db.BooleanProperty()
 
 # token: unique identifier linked to clubs (OpenID or WIND)
 class Token(db.Model):
     token = db.StringProperty()
-    # a google user, to request push rights to clubs
+    # a google user, to allow auto sign in
     user = db.UserProperty()
 
-# many-to-many
+# maps login tokens (admins) to clubs
 class Token2Club(db.Model):
     token = db.ReferenceProperty(Club, required=True, collection_name="tokens")
     club = db.ReferenceProperty(Club, required=True, collection_name="clubs")
 
+# maps emails to clubs
 class Email2Club(db.Model):
     email = db.ReferenceProperty(Email, required=True, collection_name="emails")
     club = db.ReferenceProperty(Club, required=True, collection_name="clubs")
+    # switch to disable abuse/spam from different clubs
+    enable = db.BooleanProperty(default=True)
 
 ################################################################################
 # Flyer-sending-related models
 
 class Email(db.Model):
-    id = db.StringProperty()
     email = db.StringProperty()
+    # either banhammer, or request to not have the service
     enable = db.BooleanProperty(default=True)
 
 class Flyer(db.Model):
     id = db.StringProperty()
+    # a flyer belongs to only one club
     club = db.ReferenceProperty(Club)
+    # human-readable handle - used in nag emails
     name = db.StringProperty()
+    # !!! change to blobstore, possibly in branch
     flyer = db.BlobProperty()
-    # dates
+    # timestamps
     upload_date = db.DateTimeProperty(auto_now_add=True)
     last_sent_date = db.DateTimeProperty()
     event_date = db.DateTimeProperty()
@@ -45,7 +53,6 @@ class Flyer(db.Model):
 
 # a many-many link between flyers and emails
 class Job(db.Model):
-    id = db.StringProperty()
     # references
     flyer = db.ReferenceProperty(Flyer, collection_name="jobs")
     email = db.ReferenceProperty(Email, collection_name="jobs")
